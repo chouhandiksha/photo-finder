11c54ff34d7d06fedb615c9fe64ed14ac56640fe
chouhandiksha <chouhan241@gmail.com>
2021-11-06 22:49:42 -0400

    Adding lambdas

---
+++ Lambdas/find-photo.py
@@ -0,0 +1,93 @@
+import json
+from requests_aws4auth import AWS4Auth
+import boto3
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from botocore.vendored import requests
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    labels = event['queryStringParameters']['search-labels']
+    keywords = get_label_keywords(labels)
+    print(keywords)
+    
+    
+    
+    
+    
+    host = "search-photos-gg4gdku7fjltpovyhngdpmqtj4.us-west-2.es.amazonaws.com"
+    region = "us-west-2"
+    service = "es"
+    
+    awsauth = AWS4Auth("AKIAVV2BVFTMINEZOFFJ", "ubp/IC8EXcX+hmi6pLUnQnt/zxS2qvXaINE9HvyZ", region, service)
+    es = Elasticsearch(
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
+    print(es)
+    
+    
+    endpoint = 'https://search-photos-gg4gdku7fjltpovyhngdpmqtj4.us-west-2.es.amazonaws.com'
+    headers = {'Content-Type': 'application/json'}
+    prepq = []
+    
+    
+    for lbl in labels:
+        prepq.append({"match": {"labels": lbl}})
+        
+    query = {"query": {"bool": {"should": prepq}}}
+    es_resultdata = es.search(index="object-key", body=query)
+    #r = requests.post(endpoint, headers=headers, data=json.dumps(query))
+    print(es_resultdata)
+    
+    result_img = []
+    for each in es_resultdata['hits']['hits']:
+        objKey = each['_source']['object-key']
+        bucket = "photo-bucket-b2"
+        image_url = "https://" + bucket + ".s3.amazonaws.com/" + objKey
+        result_img.append(image_url)
+        print(each['_source']['labels'])
+    print(result_img)
+    
+    
+    
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True
+        },
+        'body': json.dumps({"results": result_img})
+    }
+    
+    
+    
+    
+    
+    
+    
+def get_label_keywords(inputText):
+    lex = boto3.client('lex-runtime')
+    response = lex.post_text(
+        botName='photo_finder',
+        botAlias='findPhoto',
+        userId='user1',
+        inputText=inputText)
+    print(response)
+    # labels_keywords = []
+    # slots = response['slots']
+    # labels_keywords = [v for _, v in slots.items() if v]
+    # print(labels_keywords)
+    # return labels_keywords
+   
+        
+        
+        
+        
+        
+
+    
---
+++ Lambdas/index-photos.py
@@ -0,0 +1,100 @@
+import json
+import boto3
+from requests_aws4auth import AWS4Auth
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from datetime import datetime
+
+
+
+def lambda_handler(event, context):
+    # TODO implement
+  
+    print('This has been triggered',event)
+    s3 = boto3.resource('s3', region_name='us-west-2')#Replace with your region name
+    labels = []
+    
+    
+    client1 = boto3.client('s3',region_name = 'us-west-2')
+    
+    #Request for head object
+    
+    
+    
+    
+    customLabelsHead= []
+    
+    #Loops through every file uploaded
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        bucket = s3.Bucket(bucket_name)
+        file_name = str(record['s3']['object']['key'])
+        print(record['s3']['object'])
+    client2 = boto3.client('rekognition',region_name = 'us-west-2')
+    
+    
+    response2 = client2.detect_labels(Image = {"S3Object":{"Bucket": "photo-bucket-b2", "Name":file_name}}, MaxLabels = 10,MinConfidence= 50)
+    response1 = client1.head_object(
+    Bucket='photo-bucket-b2',
+    Key=file_name)
+    print("labels are",response1['Metadata']['customlabels'])
+    
+    
+    
+    
+    #Append detected Labels into a list
+    for res in response2['Labels']:
+        labels.append(res['Name'])
+    
+    #append custom header labels into rekognition label list
+    if customLabelsHead:
+        labels.extend(customLabelsHead)
+        
+        
+    
+    
+    
+    
+    host = "search-photos-gg4gdku7fjltpovyhngdpmqtj4.us-west-2.es.amazonaws.com"
+    region = "us-west-2"
+    service = "es"
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth("AKIAVV2BVFTMINEZOFFJ", "ubp/IC8EXcX+hmi6pLUnQnt/zxS2qvXaINE9HvyZ", region, service)
+    es = Elasticsearch(
+        hosts=[{'host': host, 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
+    
+    
+    
+    
+    
+    
+    
+    doc = {
+            "object-key": file_name,
+            "bucket": "photo-bucket-b2",
+            "createdTimeStamp" : datetime.now(),
+            "labels" : labels
+            }
+        
+    es.index(
+        index= "object-key",
+        doc_type="Labels",
+        id=file_name,
+        body=doc)
+    
+    
+    
+    
+    check = es.get(index="object-key", doc_type="Labels", id=file_name)
+    if check["found"]:
+        print("Index %s succeeded" % file_name)
+    
+    
+    print(es)
+    print(labels)
+    
+    return "Success"
